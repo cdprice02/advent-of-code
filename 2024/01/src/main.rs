a6d0dcc90f6b822e21a6ed2d275d5506f7f49743
Carson Price <carson.price@mines.sdsmt.edu>
2024-12-13 17:23:35 -0700

    Part 1 cleanup

--- 2024/01/src/main.rs
+++ 2024/01/src/main.rs
@@ -5,23 +5,23 @@ use miette::{IntoDiagnostic, Result};
 fn main() -> Result<()> {
     let file = fs::read_to_string("input.txt").into_diagnostic()?;
 
-    let mut list1 = Vec::<u32>::new();
-    let mut list2 = Vec::<u32>::new();
+    let mut list_1 = Vec::<u32>::new();
+    let mut list_2 = Vec::<u32>::new();
 
     for line in file.lines() {
         let parts = line.split_once("   ").expect("each line has two parts");
 
-        list1.push(parts.0.parse().expect("each part is a number"));
-        list2.push(parts.1.parse().expect("each part is a number"));
+        list_1.push(parts.0.parse().expect("each part is a number"));
+        list_2.push(parts.1.parse().expect("each part is a number"));
     }
 
-    list1.sort();
-    list2.sort();
+    list_1.sort();
+    list_2.sort();
 
-    let mut diff = 0;
-    for (&n1, &n2) in list1.iter().zip(list2.iter()) {
-        diff += (n1 as i64).abs_diff(n2 as i64);
-    }
+    let diff = list_1
+        .iter()
+        .zip(list_2.iter())
+        .fold(0, |acc, (&n1, &n2)| acc + (n1 as i64).abs_diff(n2 as i64));
 
     println!("{}", diff);
 
