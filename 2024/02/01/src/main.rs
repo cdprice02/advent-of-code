b233ab372c0e8c8031fb8f5193080fd9d2ff92b8
Carson Price <carson.price@mines.sdsmt.edu>
2024-12-16 12:18:28 -0700

    Part 1 renaming

--- 2024/02/01/src/main.rs
+++ 2024/02/01/src/main.rs
@@ -2,43 +2,43 @@ use std::fs;
 
 use miette::{IntoDiagnostic, Result};
 
-type Room = i32;
+type Floor = i32;
 
 fn main() -> Result<()> {
     let file = fs::read_to_string("input.txt").into_diagnostic()?;
 
-    let mut num_safe_floors = 0;
+    let mut num_safe_reports = 0;
 
     'line: for line in file.lines() {
-        let floor = line
+        let floors = line
             .split(' ')
-            .map(|x| x.parse::<Room>().expect("all rooms are numbers"))
+            .map(|x| x.parse::<Floor>().expect("all floors are numbers"))
             .collect::<Vec<_>>();
 
-        assert!(floor.len() >= 2, "floor should have at least 2 rooms");
+        assert!(floors.len() >= 2, "level should have at least 2 floors");
 
-        let init_diff = room_diff(floor[0], floor[1]);
+        let init_diff = floor_diff(floors[0], floors[1]);
         if !valid_diff(init_diff) {
             continue;
         }
 
-        for i in 1..floor.len() - 1 {
-            let diff = room_diff(floor[i], floor[i + 1]);
+        for i in 1..floors.len() - 1 {
+            let diff = floor_diff(floors[i], floors[i + 1]);
             if !check_diff(init_diff, diff) {
                 continue 'line;
             }
         }
 
-        num_safe_floors += 1;
+        num_safe_reports += 1;
     }
 
-    println!("{}", num_safe_floors);
+    println!("{}", num_safe_reports);
 
     Ok(())
 }
 
-fn room_diff(room_1: Room, room_2: Room) -> i32 {
-    room_2 - room_1
+fn floor_diff(floor_1: Floor, floor_2: Floor) -> i32 {
+    floor_2 - floor_1
 }
 
 fn check_diff(init_diff: i32, diff: i32) -> bool {
